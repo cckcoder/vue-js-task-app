b3aaf485285dd6529fcb634feeb60c333779848a
codewizz_thinkpad <codewizz@cckcoder.cc>
2023-04-28 23:35:27 +0700

    update: setup button and count number
    Signed-off-by: codewizz_thinkpad <codewizz@cckcoder.cc>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,36 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
-const myName = ref('Codewizz');
-const txtColor = ref('text-primary');
-const bgColor = ref('text-bg-success');
+const myName = ref('Codewizz')
+const txtColor = ref('text-primary')
+const bgColor = ref('text-bg-success')
 
+const count = ref(0)
 </script>
 
 <template>
-  <main>
+  <div>
     <h1 :class="bgColor">Hello World</h1>
     <h2 :class="txtColor">{{ myName }}</h2>
-  </main>
+    <hr />
+    <div class="row">
+      <div class="offset-md-4 col-md-6">
+        <button class="btn btn-primary">+</button>
+        <span class="h3">{{ count }}</span>
+        <button class="btn btn-danger">-</button>
+      </div>
+    </div>
+  </div>
 </template>
 
+<style scoped>
+
+button {
+  width: 100px;
+}
+
+span {
+  margin: 0px 10px;
+  padding: 10px;
+}
+</style>

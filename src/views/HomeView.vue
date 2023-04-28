cfee0a02f14eac2c62fd78ca517d663a12409ced
codewizz_thinkpad <codewizz@cckcoder.cc>
2023-04-28 23:20:30 +0700

    update: v-bind
    Signed-off-by: codewizz_thinkpad <codewizz@cckcoder.cc>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const myName = ref('Codewizz');
+const txtColor = ref('text-primary');
+const bgColor = ref('text-bg-success');
+
+</script>
 
 <template>
   <main>
-    <h1>Hello World</h1>
+    <h1 :class="bgColor">Hello World</h1>
+    <h2 :class="txtColor">{{ myName }}</h2>
   </main>
 </template>
+

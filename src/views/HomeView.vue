40ec5b2c5196917485ced454f8c565ec94f2997d
codewizz_thinkpad <codewizz@cckcoder.cc>
2023-04-28 23:40:30 +0700

    update: v-on short hand style
    Signed-off-by: codewizz_thinkpad <codewizz@cckcoder.cc>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,18 +10,26 @@ const count = ref(0)
 const increment = () => {
   count.value++
 }
+
+const decrement = () => {
+  count.value--
+}
+
+const helloMyName = () => {
+  alert(`Hello ${myName.value}`)
+}
 </script>
 
 <template>
   <div>
     <h1 :class="bgColor">Hello World</h1>
-    <h2 :class="txtColor">{{ myName }}</h2>
+    <h2 :class="txtColor" @click="helloMyName">{{ myName }}</h2>
     <hr />
     <div class="row">
       <div class="offset-md-4 col-md-6">
-        <button class="btn btn-primary" v-on:click="increment">+</button>
+        <button class="btn btn-primary" @click="increment">+</button>
         <span class="h3">{{ count }}</span>
-        <button class="btn btn-danger">-</button>
+        <button class="btn btn-danger" @click="decrement">-</button>
       </div>
     </div>
   </div>
@@ -35,5 +43,9 @@ button {
 span {
   margin: 0px 10px;
   padding: 10px;
+}
+
+h2 {
+  cursor: pointer; 
 }
 </style>

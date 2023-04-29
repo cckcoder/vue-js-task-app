619fba1d55e2e851241b776883a8b6daa27445c4
codewizz_thinkpad <codewizz@cckcoder.cc>
2023-04-29 13:02:21 +0700

    update: complete component
    Signed-off-by: codewizz_thinkpad <codewizz@cckcoder.cc>

---
+++ src/components/TaskList.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+const tasks = ref([
+  {
+    id: '0001',
+    title: 'Buy milk',
+    description: 'Get a gallon of milk from the store.',
+    completed: false
+  },
+  {
+    id: '0002',
+    title: 'Clean the kitchen',
+    description: 'Wash dishes and wipe down countertops and appliances.',
+    completed: false
+  },
+  {
+    id: '0003',
+    title: 'Do homework',
+    description: 'Complete assigned homework for the day.',
+    completed: false
+  },
+  {
+    id: '0004',
+    title: 'Schedule dentist appointment',
+    description: 'Call the dentist to schedule a routine cleaning.',
+    completed: false
+  },
+  {
+    id: '0005',
+    title: 'Water the plants',
+    description: 'Give the houseplants a thorough watering.',
+    completed: false
+  }
+])
+</script>
+
+<template>
+  <div class="offset-4 col-md-6 mt-3">
+    <div class="card mb-3" v-for="task in tasks" :key="task.id">
+      <div class="card-body">
+        <div class="d-flex justify-content-between">
+          <span>{{ task.title }}</span>
+          <button class="btn btn-danger btn-sm">&times;</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,18 +1,27 @@
 <script setup>
 import { computed, ref } from 'vue'
+import TaskList from '@/components/TaskList.vue'
 
 const formTask = computed(() => {
   return ['border', 'border-info', 'border-2', 'p-4', 'rounded']
 })
 
-const tasks = ref([])
+const task = ref({
+  title: '',
+  description: ''
+})
 
 const submitTask = () => {
   tasks.value.push({
     id: tasks.value.length + 1,
-    title: 'Task ' + tasks.value.length,
-    description: 'Description ' + tasks.value.length
+    title: task.value.title,
+    description: task.value.description
   })
+
+  task.value = {
+    title: '',
+    description: ''
+  }
 }
 </script>
 
@@ -24,11 +33,18 @@ const submitTask = () => {
         <div class="mb-3">
           <div class="form-group mb-3">
             <label for="task" class="form-label">Task</label>
-            <input type="text" class="form-control" placeholder="Add your Task here!" name="task" />
+            <input
+              v-model="task.title"
+              type="text"
+              class="form-control"
+              placeholder="Add your Task here!"
+              name="task"
+            />
           </div>
 
           <div class="form-group">
             <textarea
+              v-model="task.description"
               name="description"
               rows="3"
               class="form-control"
@@ -39,15 +55,7 @@ const submitTask = () => {
         </div>
       </form>
     </div>
-    <div class="offset-4 col-md-6 mt-3">
-      <div class="card">
-        <div class="card-body">
-          <div class="d-flex justify-content-between">
-            <span>Item 1</span>
-            <button class="btn btn-danger btn-sm">&times;</button>
-          </div>
-        </div>
-      </div>
-    </div>
+
+    <TaskList />
   </div>
 </template>

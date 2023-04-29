297df75e1e1be599a744144886c6b2de0e31f8f9
codewizz_thinkpad <codewizz@cckcoder.cc>
2023-04-29 11:43:01 +0700

    update: split navbar to component
    Signed-off-by: codewizz_thinkpad <codewizz@cckcoder.cc>

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Task App</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <RouterLink to="/" class="nav-link">Home</RouterLink>
-              </li>
-              <li class="nav-item">
-                <RouterLink :to="{ name: 'task' }" class="nav-link">Task</RouterLink>
-              </li>
-              <li class="nav-item">
-                <RouterLink to="/about" class="nav-link">About</RouterLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <div class="wrapper"></div>
   </header>
   <div class="container mt-3">
     <RouterView />
   </div>
 </template>
-
-<style scoped></style>

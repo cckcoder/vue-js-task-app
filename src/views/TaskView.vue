1933f034e620ea0189fdaf72b35b6f3ede34d61b
codewizz_thinkpad <codewizz@cckcoder.cc>
2023-04-29 12:47:28 +0700

    update: prepare for task component
    Signed-off-by: codewizz_thinkpad <codewizz@cckcoder.cc>

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,9 +1,19 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const formTask = computed(() => {
   return ['border', 'border-info', 'border-2', 'p-4', 'rounded']
 })
+
+const tasks = ref([])
+
+const submitTask = () => {
+  tasks.value.push({
+    id: tasks.value.length + 1,
+    title: 'Task ' + tasks.value.length,
+    description: 'Description ' + tasks.value.length
+  })
+}
 </script>
 
 <template>
@@ -12,11 +22,32 @@ const formTask = computed(() => {
     <div class="offset-4 col-md-6">
       <form @submit.prevent="submitTask" :class="formTask">
         <div class="mb-3">
-          <label for="task" class="form-label">Task</label>
-          <input type="text" class="form-control" placeholder="Add your Task here!" name="task" />
+          <div class="form-group mb-3">
+            <label for="task" class="form-label">Task</label>
+            <input type="text" class="form-control" placeholder="Add your Task here!" name="task" />
+          </div>
+
+          <div class="form-group">
+            <textarea
+              name="description"
+              rows="3"
+              class="form-control"
+              placeholder="Description"
+            ></textarea>
+          </div>
           <button type="submit" class="btn btn-primary mt-3 w-100">Submit</button>
         </div>
       </form>
+    </div>
+    <div class="offset-4 col-md-6 mt-3">
+      <div class="card">
+        <div class="card-body">
+          <div class="d-flex justify-content-between">
+            <span>Item 1</span>
+            <button class="btn btn-danger btn-sm">&times;</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
